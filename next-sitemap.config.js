9e5f5ff2eed8e0dbe619ab478dc2c0cf591e1afe
Yixuan YANG <yyx201601@gmail.com>
2025-09-17 23:51:17 +0930

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.ygfaus.com.au/',
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+    changefreq: 'weekly',
+    priority: 0.7,
+};
