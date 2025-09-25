9d2a14e28533a506cf19d507b35292824f54a16f
Yixuan YANG <yyx201601@gmail.com>
2025-09-25 19:24:46 +0930

    update client comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,19 +72,10 @@ export default function HomePage() {
             image={TestimonialsImage[0]}
             title={'What Our\nClients Say'}
             subtitle=""
-            description1={'Ryan\nExceptional food and service! Having explored numerous Mala Tang restaurants across various states, this one stands out as the best by far. The selection is extensive, with a wide variety of fresh, well-presented, and hygienic options. The soup is rich, flavourful and perfectly balanced. The staff are incredibly polite, welcoming, and attentive, adding to the overall experience. Without a doubt, this is my top choice for Mala Tang.'}
+            description1={'Xiangzhe\nDelicious food, super clean place, and really fast service. Loved it!'}
+            
             description2={'Hans\nThe best Chinese hotpot in Adelaide unless the boss decides to open a second store in city. Authentic flavours that make me coming back at least three times a week. Always five-star experience! Btw, the staff are super kind and adorable.'}
             description3={'Margaret\nA wonderful place for hot pot. Love the soup bases and the variety of fresh ingredients to use.\nMost definitely coming back.'}
-          />
-        </div>
-        <div style={{ position: 'relative' }}>
-          <TextImage13
-            image={TestimonialsImage[0]}
-            title={''}
-            subtitle=""
-            description1={'Xiangzhe\nDelicious food, super clean place, and really fast service. Loved it!'}
-            description2={'Boya\nVery delicious food, the price is also very affordable. Dry mix is a classic taste, bone soup is also very tasty. As a Chinese food well worth trying, I will come again next time. In addition, the staff are friendly and good attitude.'}
-            description3={''}
           />
         </div>
       </main>
