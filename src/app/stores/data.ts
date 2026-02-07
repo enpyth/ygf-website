171b63466c4751507f20223552309d4013ccadcd
enpyth <enpyth@outlook.com>
2026-02-07 11:33:00 +0800

    hotfix: update page data

--- src/app/stores/data.ts
+++ src/app/stores/data.ts
@@ -27,7 +27,7 @@ export const stores: Store[] = [
         iconType: 'star',
         coordinates: { lat: -34.9285, lng: 138.6007 },
         status: 'open',
-        phone: '+61 087119 8917',
+        // phone: '+61 087119 8917',
         hours: [
             'Monday - Friday: 11:00 AM - 9:00 PM',
             'Saturday - Sunday: 10:00 AM - 10:00 PM'
